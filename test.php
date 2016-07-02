1e10819297f580ca6549757fe15e3cd889049d0f
iliubang <it.liubang@qq.com>
2016-07-02 12:23:06 +0800

    debug

--- test.php
+++ test.php
@@ -1,12 +1,16 @@
 <?php
 $list = new Lb\Linklist();
 
-for ($i = 0; $i < 100; $i++) {
+for ($i = 0; $i < 10; $i++) { 
 	$list->list_add_head("hello liubang" . $i);
 }
 
+$list->list_delete_index(0);
+echo $list->list_element_nums();
+echo PHP_EOL;
+
 for ($i = 0; $i < $list->list_element_nums(); $i++) {
-	echo $list->list_fetch_index($i);
+	echo $list->list_fetch_index($i), PHP_EOL;
 }
 
 
@@ -26,8 +30,8 @@ class Demo
 
 $d = new Demo('liubang', '男');
 
-var_dump($d);
+//var_dump($d);
 
-$list->list_add_tail($d);
+$list->list_add_head($d);
 
-var_dump($list->list_fetch_tail());
+var_dump($list->list_fetch_head());
